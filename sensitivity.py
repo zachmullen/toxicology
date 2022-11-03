f7e6b29c4cdaa5ed50219b676ba5ad1011805c20
Zach Mullen <zach.mullen@kitware.com>
2022-11-03 17:20:13 -0400

    First pass at heat map view of PRCC values for all inputs/outputs

--- sensitivity.py
+++ sensitivity.py
@@ -1,10 +1,11 @@
-import math
+from itertools import product
 import streamlit as st
+import numpy as np
 import pandas as pd
 import pingouin as pg
 import plotly.express as px
 
-
+# TODO next version of st.cache will allow passing a custom message to show_spinner, use that instead.
 @st.cache(show_spinner=False)
 def df_from_file(file) -> pd.DataFrame:
     if file.name.endswith('.csv'):
@@ -17,15 +18,7 @@ def get_color_by_value(column_name: str, dict_df: pd.DataFrame) -> str:
     return dict_df[dict_df['Field Name'] == column_name].iloc[0].Category
 
 
-@st.cache(show_spinner=False)
-def run_prcc(dict_df: pd.DataFrame, data_df: pd.DataFrame, output_name: str) -> dict[str, float]:
-    input_names = dict_df.loc[dict_df['Type'] == 'Input']['Field Name'].to_list()
-    return {
-        input_name: float(pg.partial_corr(data_df, input_name, output_name, method='spearman').r)
-        for input_name in input_names
-    }
-
-
+st.set_page_config(layout="wide")
 st.title('Toxicology sensitivity analysis')
 
 upload_container = st.empty()
@@ -40,18 +33,30 @@ if data_file and dict_file:
         data_df = df_from_file(data_file)
         dict_df = df_from_file(dict_file)
 
-        output_cols = dict_df.loc[dict_df['Type'] == 'Output']['Field Name'].to_list()
+        # 1. Find all input columns
+        input_cols = dict_df.loc[dict_df['Type'] == 'Input']['Field Name']
 
-    output_name = st.selectbox('Select output parameter:', options=[''] + output_cols)
+        # 2. Ignore input columns that have the same value in every row
+        varying_input_cols = [col for col in input_cols if data_df[col].nunique() > 1]
 
-    if output_name:
-        with st.spinner(f'Running PRCC on {output_name}'):
-            results = run_prcc(dict_df, data_df, output_name)
-            result_list = [
-                [k, v, get_color_by_value(k, dict_df)]
-                for k, v in results.items() if not math.isnan(v)
-            ]
-            plot_df = pd.DataFrame(result_list, columns=['Parameter', 'PRCC', 'Category'])
-            fig = px.bar(plot_df, x='Parameter', y='PRCC', title=f'PRCC results: {output_name}', color='Category')
-            fig.update_layout(xaxis_categoryorder='total descending')
-            fig
+        # 3. Loop over all output columns with a progress bar, running prcc on each output
+        output_cols = dict_df.loc[dict_df['Type'] == 'Output']['Field Name']
+
+    progress = st.progress(0)
+    total_cells = len(varying_input_cols) * len(output_cols)
+    heatmap = np.zeros(total_cells, dtype=np.float64)
+    for i, (in_col, out_col) in enumerate(product(varying_input_cols, output_cols)):
+        progress.progress(i / total_cells)
+        prcc = pg.partial_corr(data_df, in_col, out_col, method='spearman')
+        heatmap[i] = abs(prcc.r)
+
+    heatmap = heatmap.reshape((len(varying_input_cols), len(output_cols)))
+    progress.empty()
+    fig = px.imshow(
+        heatmap,
+        labels=dict(x='Output', y='Input', color='|PRCC|'),
+        x=output_cols,
+        y=varying_input_cols,
+        height=45 * len(varying_input_cols),
+    )
+    st.plotly_chart(fig, use_container_width=True)
